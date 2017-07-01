19ce839c6b6f84eadff3a58f7b303b6013f1eb12
Piotr Leniec <leniec.piotr@gmail.com>
2017-07-01 23:04:01 +0200

    configured hmr for javascripts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 
-ReactDOM.render(
-  <Counter />,
-  document.getElementById('root')
-)
+const render = Component => {
+  ReactDOM.render(
+    <Component />,
+    document.getElementById('root')
+  )
+}
+
+render(Counter)
+
+if (module.hot) {
+  module.hot.accept('./components/Counter', () => {
+    const NextCounter = require('./components/Counter').default
+
+    render(NextCounter)
+  })
+}
